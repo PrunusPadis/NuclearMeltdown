cdb245933006d055e4d2a48cf48ce98b4dc8487d
Crare <10487241+Crare@users.noreply.github.com>
2025-08-02 13:34:02 +0300

    mainmenu assets

---
+++ Assets/Scripts/MainMenu/MainMenuAudioSlider.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+public class MainMenuAudioSlider : MonoBehaviour
+{
+    private AudioMixer _mainMixer;
+    public PhysicalSlider audioSlider;
+    public string audioMixerGroup;
+
+    public float value;
+    public float maxValue;
+    public float minValue;
+
+    public float maxRotation;
+    public float minRotation;
+
+    public int targetIndex;
+
+    public Transform target;
+
+
+    public virtual void UpdateAudioValue()
+    {
+        _mainMixer.SetFloat(audioMixerGroup, value);
+    }
+
+    protected virtual void UpdateMeterVisual()
+    {
+        float normalizedValue = value / (maxValue - minValue);
+
+        float angle = Mathf.Lerp(minRotation, maxRotation, normalizedValue);
+        target.localRotation = Quaternion.Euler(new Vector3(0, 0, angle));
+
+    }
+
+    private void Update()
+    {
+        UpdateAudioValue();
+        UpdateMeterVisual();
+
+    }
+#if UNITY_EDITOR
+    void OnValidate()
+    {
+        UpdateMeterVisual();
+    }
+
+
+#endif
+
+}
+
