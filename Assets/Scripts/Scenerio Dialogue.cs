ed810b0f168dc98a9a813e40a2617d9f873803a6
DESKTOP-BQT1UCP\Wanha <prunuspadis>
2025-08-02 17:22:28 +0300

    Added audio and text acording to scenario

--- Assets/Scripts/Scenerio Dialogue.cs
+++ Assets/Scripts/Scenerio Dialogue.cs
@@ -1,10 +1,17 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "ScenerioDialogue", menuName = "Scriptable Objects/ScenerioDialogue")]
 public class ScenerioDialogue : ScriptableObject
 {
-    public string text;
-    public AudioClip clip;
+    public List<DialoguePart> parts;
+    [Serializable]
+    public class DialoguePart
+    {
+        public string text;
+        public AudioClip clip;
 
-    public float duration;
+        public float duration;
+    }
 }
