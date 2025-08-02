0a03eb1c8cb24017511604d17c98b1b6896c5424
Crare <10487241+Crare@users.noreply.github.com>
2025-08-02 15:51:22 +0300

    main menu slider chnages

--- Assets/Scripts/MainMenu/MainMenuAudioSlider.cs
+++ Assets/Scripts/MainMenu/MainMenuAudioSlider.cs
@@ -4,9 +4,10 @@ using UnityEngine.Audio;
 
 public class MainMenuAudioSlider : MonoBehaviour
 {
-    private AudioMixer _mainMixer;
+    public AudioMixer _mainMixer;
+    public AudioMixerGroup audioMixerGroup;
     public PhysicalSlider audioSlider;
-    public string audioMixerGroup;
+    //public string audioMixerGroup;
 
     public float value;
     public float maxValue;
@@ -21,34 +22,67 @@ public class MainMenuAudioSlider : MonoBehaviour
 
     private void Start()
     {
-        InitializeAudioSettings();
+        //InitializeAudioSettings();
+        foreach (var param in _mainMixer.FindMatchingGroups(string.Empty))
+        {
+            Debug.Log("Exposed Parameter: " + param.name);
+        }
+
+        Debug.Log("_mainMixer.name: " + _mainMixer.name);
+
+        var parameters = _mainMixer.GetFloat("Master", out float value);
+        Debug.Log("Parameter found: " + parameters + " value: " + value);
+
+        var parameters2 = _mainMixer.GetFloat("MasterVolume", out float value2);
+        Debug.Log("Parameter found: " + parameters2 + " value: " + value2);
+
+        var parameters3 = _mainMixer.GetFloat("Volume", out float value3);
+        Debug.Log("Parameter found: " + parameters3 + " value: " + value3);
     }
 
-    public void InitializeAudioSettings()
-    {
-        _mainMixer = Resources.Load<AudioMixer>("NewAudioMixer");
-    }
+
+
+
+    //public void InitializeAudioSettings()
+    //{
+    //    _mainMixer = Resources.Load<AudioMixer>("NewAudioMixer");
+    //}
 
     public virtual void UpdateAudioValue()
     {
-        _mainMixer.SetFloat(audioMixerGroup, value);
+        float normalizedValue = value / (maxValue - minValue);
+
+        float dbVolume = Mathf.Log10(normalizedValue) * 20;
+        if (value == 0.0f) dbVolume = -80.0f;
+        //Debug.Log("audioMixerGroup: " + audioMixerGroup);
+        //_mainMixer.SetFloat(audioMixerGroup.name+"Volume", dbVolume);
+        _mainMixer.SetFloat(audioMixerGroup.name, dbVolume);
     }
 
     protected virtual void UpdateMeterVisual()
     {
         float normalizedValue = value / (maxValue - minValue);
 
-        float angle = Mathf.Lerp(minRotation, maxRotation, normalizedValue);
+        float normalizedRotation = normalizedValue / (maxRotation - minRotation);
+
+        float angle = Mathf.Lerp(minRotation, maxRotation, normalizedRotation);
         target.localRotation = Quaternion.Euler(new Vector3(0, 0, angle));
 
     }
 
-    private void Update()
+    public void ApplyChange(float f)
     {
+        value = f;
         UpdateAudioValue();
         UpdateMeterVisual();
-
     }
+
+    //private void Update()
+    //{
+    //    UpdateAudioValue();
+    //    UpdateMeterVisual();
+    //}
+
 #if UNITY_EDITOR
     void OnValidate()
     {
