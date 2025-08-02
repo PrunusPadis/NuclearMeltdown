b3639fa968f53a65b140ab7ec0b2d9f33b38b6e0
Crare <10487241+Crare@users.noreply.github.com>
2025-08-02 17:32:47 +0300

    sliders to textdisplayer

--- Assets/Scripts/Physical Slider.cs
+++ Assets/Scripts/Physical Slider.cs
@@ -5,10 +5,12 @@ using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 using static UnityEngine.Rendering.DebugUI;
 
-public class PhysicalSlider : ComplexSwitch
+public class PhysicalSlider : ComplexSwitch, IPointerEnterHandler, IPointerExitHandler
 {
 
 
+    public UnityEvent PointerEntered = new();
+    public UnityEvent PointerExited = new();
 
     public Transform maxPosAnchor;
     public Transform minPosAnchor;
@@ -21,5 +23,19 @@ public class PhysicalSlider : ComplexSwitch
     protected override float UpdateValue(Vector2 delta)
     {
         return delta.y * sensitivity; 
+    }
+
+
+
+    public virtual void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("PhysicalSlider OnPointerEnter");
+        PointerEntered.Invoke();
+    }
+
+    public virtual void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("PhysicalSlider OnPointerExit");
+        PointerExited.Invoke();
     }
 }
